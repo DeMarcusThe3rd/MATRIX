74645a2d0ecaef520b0fb11b8b0a09771c688a95
Marcus <marcusyeww@gmail.com>
2024-10-29 16:03:16 +0800

    print matrix aesthetically~

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <iomanip>
 #include "matrix.h"
-#include <vector>
 
 using namespace std;
 
 int main(){
     Matrix matrix;
-    matrix.set(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0);
+    matrix.set(1.2246,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0);
     matrix.print();
 
 
--- matrix.cpp
+++ matrix.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 #include <iomanip>
 #include "matrix.h"
-#include <vector>
 
 using namespace std;
 
 void Matrix::print(){
-    int row;
-    int col;
-
-    for (row =0;row<3;row++){
-        for (col=0;col<3;col++){
-            cout << x[row][col] <<endl;
+    for (int row =0;row<3;row++){ 
+        for (int col=0;col<3;col++){
+            cout << "[" << fixed << setprecision(3) << x[row][col] << "]";
+            cout << "  ";
         }
+        cout << endl;
     }
 }
 
 void Matrix::set(double a,double b,double c,double d,double e,double f,double g,double h,double i){ //set all elements of the matrix 
     double values[9] = {a,b,c,d,e,f,g,h,i};
     int index =0;
-    int row;
-    int col;
 
-    for (row =0;row<3;row++){
-        for (col=0;col<3;col++){
+    for (int row =0;row<3;row++){
+        for (int col=0;col<3;col++){
             x[row][col] = values[index++];
         }
     }
