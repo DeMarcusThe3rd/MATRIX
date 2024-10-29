517ac15eeae4a71c0905401efc6eba95c80d95ea
Marcus <marcusyeww@gmail.com>
2024-10-29 23:13:21 +0800

    tranposed and Matrix A

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@
 using namespace std;
 
 int main(){
-    Matrix matrix;
-    matrix.set(1.2246,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0);
-    matrix.print();
-
-
+    Matrix A;
+    A.set(1.2246,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0);
+    A.print();
+    cout << "\n";
+    A.set(2,2,4.041);
+    A.print();
+    //double Elem= A.getElem(2,2);
+    cout << endl;
+    A.transpose();
+    A.print();
     return 0;
 }
