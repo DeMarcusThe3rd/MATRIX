088c39c406e822460568a22a63cff127e0de7334
Marcus <marcusyeww@gmail.com>
2024-10-31 23:52:00 +0800

    inverse function call

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main(){
     Matrix A;
     Matrix B;
-    A.set(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0);
+    A.set(2.0,-5.0,3.0,1.0,7.0,-2.0,-1.0,4.0,1.0);
     B.set(7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0);
     //double result = A.rcMult(B,0,0);
-    A.sMult(4);
+    A.inv();
     A.print();
     return 0;
 }
