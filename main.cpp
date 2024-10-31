0d82e41f5908220c99d9dd9a46d541aa10904f49
Marcus <marcusyeww@gmail.com>
2024-10-31 20:42:38 +0800

    multplication function call

--- main.cpp
+++ main.cpp
@@ -8,18 +8,11 @@ using namespace std;
 
 int main(){
     Matrix A;
-    Matrix AT;
+    Matrix B;
     A.set(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0);
-    A.print();
-    A.print();
-    A.transpose();
-    //double Elem= A.getElem(2,2);
-    A.save("matrixA.txt");
-    AT.load("matrixA.txt");
-    AT.print();
-    A.add(AT);
-    A.print();
-    A.sub(AT);
+    B.set(7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0);
+    //double result = A.rcMult(B,0,0);
+    A.sMult(4);
     A.print();
     return 0;
 }
